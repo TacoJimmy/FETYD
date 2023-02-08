28546bdec0c061abeec90c683b2a478f27f00324
TacoJimmy <t5669016@ntut.org.tw>
2023-02-08 17:38:43 +0800

    Update MQTT_Test.py

--- MQTT_Test.py
+++ MQTT_Test.py
@@ -387,7 +387,7 @@ if __name__ == '__main__':
 
     while True:  
 
-        print (get_FirePeople())
+        print (get_temphumi())
         time.sleep(10)
         '''
         schedule.run_pending()  
